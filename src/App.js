f7dfcee2332d1ca901f999ba66a2ec2896845d78
Thaisa Lima <thaisafc.lima@outlook.com>
2022-02-25 08:58:55 +0100

    sending initial changes.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import About from "./screens/About";
+import Contact from "./screens/Contact";
+import Hero from "./screens/Hero";
+import Projects from "./screens/Projects";
+import Tech from "./screens/Tech";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero />
+      <About />
+      <Projects />
+      <Tech />
+      <Contact />
     </div>
   );
 }
