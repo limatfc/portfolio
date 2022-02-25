311a15e22a2c624f2fab03b0fe65739c9b87c515
Thaisa Lima <thaisafc.lima@outlook.com>
2022-02-25 15:33:17 +0100

    created hero for mobiles, including all the css styling.

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,11 +1,18 @@
+import navigationBarLogo from "../assets/images/navigation-bar-logo.png";
+import classes from "../styles/components/Navigation.module.css";
+
 export default function NavigationBar() {
   return (
-    <div className="navigation-bar">
-      <button>About</button>
-      <button>Projects</button>
-      <span>logo</span>
-      <button>Tech</button>
-      <button>Contact</button>
+    <div className={classes.navigationBarWrapper}>
+      <button className={classes.linkLike}>About</button>
+      <button className={classes.linkLike}>Projects</button>
+      <img
+        className={classes.logo}
+        src={navigationBarLogo}
+        alt="a logo containing an address book in the middle and Thaisa Lima written under it. It is wrapped in a blue frame."
+      />
+      <button className={classes.linkLike}>Tech</button>
+      <button className={classes.linkLike}>Contact</button>
     </div>
   );
 }
--- src/screens/Hero.jsx
+++ src/screens/Hero.jsx
@@ -1,20 +1,23 @@
 import NavigationBar from "../components/NavigationBar";
-import heroMainImage from "../assets/hero-main-image.png";
-import heroHoverImage from "../assets/hero-hover-image.png";
+import heroMainImage from "../assets/images/hero-main-image.png";
+import heroHoverImage from "../assets/images/hero-hover-image.png";
+import classes from "../styles/screens/Hero.module.css";
 
 export default function Hero() {
   return (
-    <section className="hero-wrapper">
+    <section className={classes.heroWrapper}>
       <NavigationBar />
       <img
+        className={classes.hoverImage}
         src={heroHoverImage}
         alt="welcome written inside a black frame. The frame is hanging from a nail."
       />
       <img
+        className={classes.mainImage}
         src={heroMainImage}
         alt="a girl with a ponytail using a green computer and making anotations on a green notebook."
       />
-      <h1>
+      <h1 className={classes.text}>
         Hello, my name is Thaísa, and I am a frontend developer from Brazil.
       </h1>
     </section>
