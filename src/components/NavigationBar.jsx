0cfd5147fea2e20df85530332f436c7277a6a9c7
Thaisa Lima <thaisafc.lima@outlook.com>
2022-02-25 11:38:40 +0100

    adding files for component creation

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,11 @@
+export default function NavigationBar() {
+  return (
+    <div className="navigation-bar">
+      <button>About</button>
+      <button>Projects</button>
+      <span>logo</span>
+      <button>Tech</button>
+      <button>Contact</button>
+    </div>
+  );
+}
--- src/screens/Hero.jsx
+++ src/screens/Hero.jsx
@@ -1,11 +1,22 @@
+import NavigationBar from "../components/NavigationBar";
+import heroMainImage from "../assets/hero-main-image.png";
+import heroHoverImage from "../assets/hero-hover-image.png";
+
 export default function Hero() {
   return (
-    <div className="navigation-bar">
-      <button>About</button>
-      <button>Projects</button>
-      <span>logo</span>
-      <button>Tech</button>
-      <button>Contact</button>
-    </div>
+    <section className="hero-wrapper">
+      <NavigationBar />
+      <img
+        src={heroHoverImage}
+        alt="welcome written inside a black frame. The frame is hanging from a nail."
+      />
+      <img
+        src={heroMainImage}
+        alt="a girl with a ponytail using a green computer and making anotations on a green notebook."
+      />
+      <h1>
+        Hello, my name is Thaísa, and I am a frontend developer from Brazil.
+      </h1>
+    </section>
   );
 }
