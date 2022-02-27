aa4c9195a4821144d88fae4a5340815b97e579c4
Thaisa Lima <thaisafc.lima@outlook.com>
2022-02-27 14:03:20 +0100

    finished tech with all styles.

--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -1,3 +1,21 @@
+import {
+  BsGithub,
+  BsLinkedin,
+  BsFillTelephoneInboundFill,
+} from "react-icons/bs";
+import { AiFillInstagram } from "react-icons/ai";
+import { HiLocationMarker } from "react-icons/hi";
+import { MdEmail } from "react-icons/md";
+
 export default function Contact() {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <BsGithub />
+      <AiFillInstagram />
+      <BsLinkedin />
+      <BsFillTelephoneInboundFill />
+      <HiLocationMarker />
+      <MdEmail />
+    </section>
+  );
 }
