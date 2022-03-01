96fdf165019afbfb2793d450b88e2e1d0633a9fc
Thaisa Lima <thaisafc.lima@outlook.com>
2022-03-01 12:03:09 +0100

    adding new images to the image folder.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,18 +1,24 @@
 import classes from "../styles/components/ProjectCard.module.css";
 
 export default function ProjectCard({ projectData, onClick }) {
-  const { id, title, comingSoon, cardImageURL } = projectData;
+  const { id, title, comingSoon, cardImageComingSoonURL, cardImageURL } =
+    projectData;
 
   function onClickHandler() {
     if (comingSoon === true) return;
     onClick(id);
   }
 
+  let displayedImage = "";
+  comingSoon === true
+    ? (displayedImage = cardImageComingSoonURL)
+    : (displayedImage = cardImageURL);
+
   return (
     <div onClick={onClickHandler} className={classes.cardWrapper}>
       <img
         className={`${classes.cardImage} ${comingSoon && classes.opacicity}`}
-        src={cardImageURL}
+        src={displayedImage}
         alt="ilustration related to the project title."
       />
       <h3 className={classes.title}>{title}</h3>
